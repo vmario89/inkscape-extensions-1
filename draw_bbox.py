4467bbeb7a240c5024943f26452bc7a50fd3b985
Mario Voigt <opensource@leyghis.de>
2020-07-29 19:54:05 +0200

    Update for InkScape 1.0

--- draw_bbox.py
+++ draw_bbox.py
@@ -1,58 +1,23 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-import sys
-sys.path.append('/usr/share/inkscape/extensions')
+import inkex
+from lxml import etree
 
-from collections import defaultdict
-
-from inkex import Effect as InkscapeEffect
-from inkex import etree, addNS
-
-from simpletransform import computeBBox, applyTransformToNode
-from simplestyle import formatStyle
-from simplepath import parsePath, translatePath, formatPath
-
-class DrawBBoxes(InkscapeEffect):
+class DrawBBoxes(inkex.Effect):
     def __init__(self):
-        InkscapeEffect.__init__(self)
-        self.filename = sys.argv[-1]
+        inkex.Effect.__init__(self)
 
     def effect(self):
-        if len(self.selected) > 0:
-            bboxes = self.calculate_bboxes(self.selected)
+        if len(self.svg.selected) > 0:
+            bboxes = [(id, node, node.bounding_box()) for id, node in self.svg.selected.items()]
             for id, node, bbox in bboxes:
-                self.draw_bbox(bbox)
+                attribs = {
+                    'style'     : str(inkex.Style({'stroke':'#ff0000','stroke-width'  : '1','fill':'none'})),
+                    'x'         : str(bbox.left),
+                    'y'         : str(bbox.top),
+                    'width'     : str(bbox.width),
+                    'height'    : str(bbox.height),
+                }
+                etree.SubElement(self.svg.get_current_layer(), inkex.addNS('rect','svg'), attribs )
 
-    def calculate_bboxes(self, nodes):
-        bboxes = [(id, node, computeBBox([node]))
-                for id, node in nodes.items()]
-
-        return bboxes
-    
-    def draw_bbox(self, bbox):
-        (x1, x2, y1, y2) = bbox
-        width = x2 - x1
-        height = y2 - y1
-        self.draw_rect(x1, y1, width, height)
-
-    #SVG element generation routine
-    def draw_rect(self, x, y, width, height):
-        layer = self.current_layer
-
-        style = {   'stroke'        : '#ff0000',
-                    'stroke-width'  : '1',
-                    'fill'          : 'none',
-        }
-                    
-        attribs = {
-            'style'     : formatStyle(style),
-            'x'         : str(x),
-            'y'         : str(y),
-            'width'     : str(width),
-            'height'    : str(height),
-        }
-
-        rect = etree.SubElement(layer, addNS('rect','svg'), attribs )
-
-effect = DrawBBoxes()
-effect.affect()
+DrawBBoxes().run()
